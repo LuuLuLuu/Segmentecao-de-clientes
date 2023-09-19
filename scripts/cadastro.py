c337d1348eb22c8e596eb326b11db24b7924b51a
Alexandre Coimbra <alexandrewcoimbra@gmail.com>
2023-09-19 19:16:49 -0300

    realizando cadastro

--- scripts/cadastro.py
+++ scripts/cadastro.py
@@ -1,8 +1,13 @@
+from main import dicionario_senhas
+
+
 def cadastro():
-    # login_usuario_novo = input("Informe seu login: ")
-    # senha_usuario_novo = input("Informe sua senha")
-    # dicionario_senhas[login_usuario_novo] = senha_usuario_novo
-    # print("Usuário cadastrado com sucesso!")
-    #
-    # Iterar ao dicionário
-    ...
+
+    login_usuario_novo = input("Informe seu login: ")
+    senha_usuario_novo = input("Informe sua senha")
+    dicionario_senhas[login_usuario_novo] = senha_usuario_novo
+    if login_usuario_novo in dicionario_senhas:
+        print("Esse usuário já está cadastrado.")
+        return
+    else:
+        print("Usuário cadastrado com sucesso!")
