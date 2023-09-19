52b5edd717041b625012621d7707fa150d5a7fef
Mateus Hennich <abacateshow@gmail.com>
2023-09-19 19:19:40 -0300

    feat: adicionando interação com o usuário no menu

--- main.py
+++ main.py
@@ -40,5 +40,50 @@ while True:
         print("Opção incorreta, tente novamente.")
         continue
 
-# Menu:
-exibir_menu()
+while True:
+    # Menu:
+    exibir_menu()
+
+    # Recebe o input do usuário para definir sua escolha no menu
+    opcao = int(input("Digite sua escolha: "))
+
+    match opcao:
+        case 1: """Realizar Cadastro"""
+
+        case 2: """Consulta Cliente"""
+
+        case 3:
+            print("""
+Cliente A:
+    prazo de pagamento - 90D
+    negociação premium
+    frete grátis em qualquer valor
+    cashback 10% do valor total de todos os pedidos
+
+Cliente B:
+    prazo de pagamento - 60D
+    frete grátis acima de R$150,00
+    cashback 5% do valor total de todos os pedidos
+
+Cliente C:
+    prazo de pagamento - 30D
+    frete grátis acima de R$1000,00
+    
+Cliente D:
+    prazo de pagamento - 10D
+    frete grátis acima de R$1500,00
+""")
+
+        case 4:
+            print("""
+Critérios para cada classe:
+Cliente A - Faturamento mensal maior que R$10.000,00
+Cliente B - Faturamento mensal maior que R$8.000,00
+Cliente C - Faturamento mensal maior que R$5.000,00
+Cliente D - Faturamento mensal maior que R$3.000,00
+""")
+        case 5: """alterar senha"""
+
+        case 6: break  # Sair
+
+        case _: print("Digite um número de 1 a 6.")
