383d8c6e63612ac4aea9d3aefd3b8ceb96dc58e8
Alexandre Coimbra <alexandrewcoimbra@gmail.com>
2023-11-13 19:26:18 -0300

    fix: :bug: fixing css bug

--- static/js/main.js
+++ static/js/main.js
@@ -7,10 +7,8 @@ $(document).ready(function () {
     const $faturamentoInput = $("#faturamentoInput")
     const usedIDs = [];
 
-    const createIconButton = (iconClass, btnClasses, clickHandler) => {
-        const $button = $("<button></button>").addClass(btnClasses);
-        const $icon = $("<i></i>").addClass(iconClass);
-        $button.append($icon);
+    const createIconButton = (btnClasses, clickHandler) => {
+        const $button = $("<span></span>").addClass(btnClasses);
         $button.click(clickHandler);
         return $button;
     };
@@ -35,10 +33,21 @@ $(document).ready(function () {
 		const $cnpjData = $("<td></td>").text(cnpj);
 		const $razaoSocialData = $("<td></td>").text(razaoSocial);
 		const $faturamentoData = $("<td></td>").text(faturamento);
+        const $classe = $("<td></td>")
 
+        const $faturamentoDecimal = parseFloat(faturamento)
 
-
-        const $editButton = createIconButton("bi bi-pencil-square", "btn btn-info btn-sm", () => {
+        if ($faturamentoDecimal >= 10000) {
+            $classe.text("Classe A")
+        } else if ($faturamentoDecimal >= 8000) {
+            $classe.text("Classe B")
+        } else if ($faturamentoDecimal >= 5000) {
+            $classe.text("Classe C")
+        } else {
+            $classe.text("Classe D")
+        }
+        
+        const $editButton = createIconButton("bi bi-pencil-square btn", () => {
             while (true) {
                 const editedRazaoSocialText = prompt("Novo nome da companhia", razaoSocial);
                 if (editedRazaoSocialText !== null) {
@@ -63,6 +72,16 @@ $(document).ready(function () {
                 const editedFaturamentoText = prompt("Novo faturamento", faturamento);
                 if (editedFaturamentoText !== null) {
                     $faturamentoData.text(editedFaturamentoText);
+                    const $faturamentoDecimalEdit = parseFloat(editedFaturamentoText)
+                    if ($faturamentoDecimalEdit >= 10000) {
+                        $classe.text("Classe A")
+                    } else if ($faturamentoDecimalEdit >= 8000) {
+                        $classe.text("Classe B")
+                    } else if ($faturamentoDecimalEdit >= 5000) {
+                        $classe.text("Classe C")
+                    } else {
+                        $classe.text("Classe D")
+                    }
                     break
                 } else {
                     alert("Você deve informar um faturamento válido.")
@@ -71,11 +90,11 @@ $(document).ready(function () {
             }
         });
 
-        const $deleteButton = createIconButton("bi bi-person-dash", "btn btn-info btn-sm", () => $newClient.remove());
+        const $deleteButton = createIconButton("bi bi-person-dash btn", () => $newClient.remove());
 
-        const $buttonsContainer = $("<td></td>").addClass("btn");
+        const $buttonsContainer = $("<td></td>")
         $buttonsContainer.append($editButton, $deleteButton);
-        $newClient.append($idData, $cnpjData, $razaoSocialData, $faturamentoData, $buttonsContainer);
+        $newClient.append($idData, $cnpjData, $razaoSocialData, $classe, $faturamentoData, $buttonsContainer);
 
         $(`#tableId`).append($newClient);
 	};
@@ -92,29 +111,5 @@ $(document).ready(function () {
             $form.addClass("was-validated");
         }
     });
-
-    // $searchInput.on("input", () => {
-    //     const searchTerm = $searchInput.val().toLowerCase();
-
-    //     $(".taskboard-item").each(function () {
-    //         const $task = $(this);
-    //         const $description = $task.find("span").text().toLowerCase();
-    //         const $dateAux = $task.find(".filter-aux")
-
-    //         if (!$description.startsWith(searchTerm)) {
-    //             $task.hide();
-    //             $dateAux.hide()
-    //         } else {
-    //             $task.show();
-    //             $dateAux.show()
-    //         }
-    //     });
-    // });
-
-    // $(window).on("beforeunload", () => {
-    //     saveTasksToLocalStorage();
-    // });
-
-    // loadTasksFromLocalStorage();
 
 });
