d0ab75db25669a32bde81f8d227ba115e36fa8ad
Mateus Hennich <99285139+anchioro@users.noreply.github.com>
2023-12-04 20:23:30 -0300

    Add files via upload

---
+++ documentacao/cadastro_classe.sql
@@ -0,0 +1,8 @@
+INSERT INTO classe (id, nome_classe, prazo_pagamento, frete_minimo, cashback, negociacao_premium, valor_maximo, valor_minimo)
+VALUES
+(0, 'Sem Classe', 0, 0, NULL, NULL, 3000, 0),
+(1, 'Classe A', 90, 0, 10, 'incluso', NULL, 10000),
+(2, 'Classe B', 60, 150, 5, 'excluso', 8000, 5000),
+(3, 'Classe C', 30, 1000, 0, 'excluso', 5000, 3000),
+(4, 'Classe D', 10, 1500, 0, 'excluso', 3000, 3000);
+
